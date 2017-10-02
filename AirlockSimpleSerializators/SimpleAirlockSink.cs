b7a013321c524fe51c53e33d2b267acc676d3c32
andrew <Andrew.Kostousov@gmail.com>
2017-10-02 14:46:35 +0500

    transfer code from vostok/airclock

---
+++ AirlockSimpleSerializators/SimpleAirlockSink.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Vostok.Airlock;
+using Vostok.Commons.Binary;
+
+namespace AirlockSimpleSerializators
+{
+    public class SimpleAirlockSink : IAirlockSink
+    {
+        public SimpleAirlockSink(Stream stream)
+        {
+            WriteStream = stream;
+            Writer = new SimpleBinaryWriter(stream);
+        }
+
+        public Stream WriteStream { get; }
+        public IBinaryWriter Writer { get; }
+    }
+}
