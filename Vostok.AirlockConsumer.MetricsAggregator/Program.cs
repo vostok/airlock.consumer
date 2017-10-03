2b9e065b72706b48fa01050708cfc074bf2f1b56
ezsilmar <zhirov@skbkontur.ru>
2017-10-03 16:45:22 +0500

    added projects for MetricsAggregator

---
+++ Vostok.AirlockConsumer.MetricsAggregator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vostok.AirlockConsumer.MetricsAggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
