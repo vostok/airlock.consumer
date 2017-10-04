cca34eb979a5258ea3f1103f00c352b7056ec988
andrew <Andrew.Kostousov@gmail.com>
2017-10-04 10:13:49 +0500

    remove trash

--- Vostok.AirlockConsumer.Logs/AirlockLogEventSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Vostok.AirlockConsumer.Logs
-{
-    public class AirlockLogEventSettings
-    {
-        public int BatchSize { get; set; } = 1000;
-        public string[] ElasticUriList { get; set; }
-    }
-}
