b8e0e58a9a6d8f8566224e938739b288ae15e70b
lapin <lapin@kontur.ru>
2017-10-12 17:14:23 +0400

    added some issues for TODOs 3

--- Vostok.AirlockConsumer/ConsumerGroupHost.cs
+++ Vostok.AirlockConsumer/ConsumerGroupHost.cs
@@ -11,7 +11,7 @@ namespace Vostok.AirlockConsumer
 {
     // todo (avk, 11.10.2017): review logging levels for expected events
     // todo (avk, 09.10.2017): integration tests for airlock consumer machinery https://github.com/vostok/airlock.consumer/issues/4
-    // todo (avk, 06.10.2017): handle kafka consumer exceptions (introduce decorator)
+    // todo (avk, 06.10.2017): handle kafka consumer exceptions (introduce decorator) https://github.com/vostok/airlock.consumer/issues/19
     public class ConsumerGroupHost : IDisposable
     {
         private readonly ConsumerGroupHostSettings settings;
