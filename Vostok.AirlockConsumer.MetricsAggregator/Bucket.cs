e722f390ba18c88c72fd2c9fab6a1b6778bb76fd
lapin <lapin@kontur.ru>
2017-10-17 11:30:28 +0400

    remove todo

--- Vostok.AirlockConsumer.MetricsAggregator/Bucket.cs
+++ Vostok.AirlockConsumer.MetricsAggregator/Bucket.cs
@@ -8,7 +8,6 @@ using Vostok.Metrics.Meters;
 
 namespace Vostok.AirlockConsumer.MetricsAggregator
 {
-    //TODO @ezsilmar Unit test this https://github.com/vostok/airlock.consumer/issues/13
     public class Bucket : IBucket
     {
         private readonly IReadOnlyDictionary<string, string> tags;
