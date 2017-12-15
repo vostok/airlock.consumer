0b90d7bbc0cb9a34d0540aefe56608b379b7c1cc
lapin <lapin@kontur.ru>
2017-12-15 10:54:03 +0400

    rename AggregationTests to TraceEventsTests and remove app events from this class

--- Vostok.AirlockConsumer.IntergationTests/TraceEventsTests.cs
+++ Vostok.AirlockConsumer.IntergationTests/TraceEventsTests.cs
@@ -3,27 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Vostok.Airlock;
-using Vostok.Metrics;
 using Vostok.Tracing;
 
 namespace Vostok.AirlockConsumer.IntergationTests
 {
-    public class AggregationTests
+    public class TraceEventsTests
     {
         private static readonly string tracesRoutingKey = RoutingKey.Create(IntegrationTestsEnvironment.Project, IntegrationTestsEnvironment.Environment, nameof(TracingTests), RoutingKey.TracesSuffix);
-        private static readonly string appEventsRoutingKey = RoutingKey.Create(IntegrationTestsEnvironment.Project, IntegrationTestsEnvironment.Environment, nameof(TracingTests), RoutingKey.AppEventsSuffix);
 
         [Test]
         [Category("Load")]
-        public void PushManyHttpSpansToAirlock()
+        public void PushManyTraceEventsToAirlock()
         {
             PushToAirlock(GenerateSpans(count: 1000));
-        }
-        [Test]
-        [Category("Load")]
-        public void PushAppEventsToAirlock()
-        {
-            PushToAirlock(GenerateMetrics(count: 100));
         }
 
         private static Span[] GenerateSpans(int count)
@@ -46,27 +38,10 @@ namespace Vostok.AirlockConsumer.IntergationTests
                     }
                 }).ToArray();
         }
-        private static MetricEvent[] GenerateMetrics(int count)
-        {
-            var utcNow = DateTimeOffset.UtcNow;
-            var random = new Random();
-            return Enumerable.Range(0, count)
-                .Select(i => new MetricEvent
-                {
-                    Timestamp = utcNow.AddSeconds(-i * 10),
-                    Values = new Dictionary<string, double> { ["testval"] = random.Next(20) },
-                    Tags = new Dictionary<string, string> { [MetricsTagNames.Host] = "testhost" },
-                }).ToArray();
-        }
 
         private static void PushToAirlock(Span[] spans)
         {
             IntegrationTestsEnvironment.PushToAirlock(tracesRoutingKey, spans, e => e.BeginTimestamp);
-        }
-
-        private static void PushToAirlock(MetricEvent[] spans)
-        {
-            IntegrationTestsEnvironment.PushToAirlock(appEventsRoutingKey, spans, e => e.Timestamp);
         }
 
     }
