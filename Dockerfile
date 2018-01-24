af2b3780aad1f55ab27a28f96c2f391a98cf5ab7
lapin <lapin@kontur.ru>
2018-01-24 16:01:10 +0400

    fix build

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,12 @@ FROM microsoft/dotnet:2.0-sdk-jessie AS build-env
 WORKDIR /app
 
 # copy csproj and restore as distinct layers
-COPY nuget.config ./
+#COPY nuget.config ./
 COPY . ./
-RUN dotnet restore
 
 # copy everything else and build
-COPY . ./
-RUN dotnet publish -c Release -o out
+#COPY . ./
+#RUN dotnet publish -c Release -o out
 
 # build runtime image
 FROM microsoft/dotnet:2.0-runtime-jessie
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd ..
+curl https://github.com/skbkontur/cement/releases/download/v1.0.22/e6257f9699a456f4d1626424ab90d2cb27337188.zip -O -J -L
+mkdir ./cement
+unzip -o e6257f9699a456f4d1626424ab90d2cb27337188.zip -d ./cement
+cm init
+curl https://raw.githubusercontent.com/vostok/cement-modules/master/settings -O -J -L
+/bin/cp ./settings ~/.cement/
+cd $OLDPWD
+mono ../cement/dotnet/cm.exe update-deps
+mono ../cement/dotnet/cm.exe build-deps -v
+mono ../cement/dotnet/cm.exe build -v
+dotnet publish -c Release -o out
