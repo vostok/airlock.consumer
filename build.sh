1c38991604e6671f4cc7d98d8054ff7020e67b26
lapin <lapin@kontur.ru>
2018-02-09 16:30:26 +0400

    fix docker build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd ..
 curl https://github.com/skbkontur/cement/releases/download/v1.0.22/e6257f9699a456f4d1626424ab90d2cb27337188.zip -O -J -L
 mkdir ./cement
 unzip -o e6257f9699a456f4d1626424ab90d2cb27337188.zip -d ./cement
-cm init
+mono ../cement/dotnet/cm.exe init
 curl https://raw.githubusercontent.com/vostok/cement-modules/master/settings -O -J -L
 /bin/cp ./settings ~/.cement/
 cd $OLDPWD
