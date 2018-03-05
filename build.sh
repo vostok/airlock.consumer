3012d07aeffe316b864e3ed7b9371d14598d0262
lapin <lapin@kontur.ru>
2018-03-05 15:50:27 +0400

    fix cement install

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ cd ..
 curl https://github.com/skbkontur/cement/releases/download/v1.0.28/19fce5bb72c28c461c2114310eceaa6ed85be457.zip -O -J -L
 mkdir ./cement
 unzip -o 19fce5bb72c28c461c2114310eceaa6ed85be457.zip -d ./cement
-mono ../cement/dotnet/cm.exe reinstall
-mono ../cement/dotnet/cm.exe init
+mono ./cement/dotnet/cm.exe reinstall
+mono ./cement/dotnet/cm.exe init
 curl https://raw.githubusercontent.com/vostok/cement-modules/master/settings -O -J -L
 /bin/cp ./settings ~/.cement/
 cd $OLDPWD
